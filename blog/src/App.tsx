52b1005dd9d51f313af371be70a2767b5dae278f
chosooyeon <1whtndus@naver.com>
2023-10-03 17:52:27 +0900

    [blog] 라우터 추가하기

--- blog/src/App.tsx
+++ blog/src/App.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes, Navigate, Link } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <ul>
+      <li><Link to='/'>Home</Link></li>
+      <li><Link to='/post'>Post</Link></li>
+      <li><Link to='/post/:id'>Post Detail</Link></li>
+      <li><Link to='/post/new'>Post New</Link></li>
+      <li><Link to='/post/edit/:id'>Post Edit</Link></li>
+      <li><Link to='/profile'>Profile</Link></li>
+    </ul>
+    <Routes>
+      <Route path='/' element={<h1>home</h1>}></Route>
+      <Route path='/post' element={<h1>Post</h1>}></Route>
+      <Route path='/post/:id' element={<h1>Post Detail</h1>}></Route>
+      <Route path='/post/new' element={<h1>Post New</h1>}></Route>
+      <Route path='/post/edit/:id' element={<h1>Post Edit</h1>}></Route>
+      <Route path='/profile' element={<h1>Profile</h1>}></Route>
+
+      {/* 다른 페이지에 갔을 때 강제로 root 페이지로 이동시켜준다 */}
+      <Route path='*' element={<Navigate replace to='/'/>}></Route>
+    </Routes>
+    </>
   );
 }
 
